4a6ce79fa3edb682308a114d5258259857306b74
Joel Kwong <joelckwong@yahoo.com>
2025-07-27 10:25:22 -0400

    added output.tf

---
+++ main.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "myredhat8" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["rhel8-stig-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["900892206871"]
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true  # Ensure public IPs are automatically assigned to instances in this subnet
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "allow_ssh_https" {
+  name        = "allow_ssh_https"
+  description = "Allow SSH and HTTPS"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.myredhat8.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh_https.id]
+  key_name               = "my-key-pair" # Replace with your SSH key pair name
+
+  tags = {
+    Name = "EC2WithOpenPorts"
+  }
+}
+
+resource "aws_eip" "this" {
+  instance = aws_instance.web.id
+  domain = "vpc"
+}
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "instance_public_dns" {
+  value = aws_instance.web.public_dns
+}
